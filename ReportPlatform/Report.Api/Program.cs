4745f10dcff499785b1a955c345ca48a79782c0b
Nguyen Thanh Phat <92991374+ITDevPhat@users.noreply.github.com>
2026-05-26 12:12:01 +0700

    Add snapshot-based preview resolver and My Reports preview flow

--- ReportPlatform/Report.Api/Program.cs
+++ ReportPlatform/Report.Api/Program.cs
@@ -27,6 +27,7 @@ using Report.QueryEngine.Artifacts;
 using Report.Infrastructure.Artifacts;
 using Report.Infrastructure.Persistence;
 using Report.Contracts.Artifacts;
+using Telerik.Reporting.Services;
 
 
 
@@ -181,6 +182,8 @@ builder.Services.AddScoped<QueryExecutionValidator>();
 builder.Services.AddScoped<DatasetMetadataService>();
 builder.Services.AddScoped<ExpressionValidationService>();
 builder.Services.AddScoped<ITelerikReportFactory, DynamicTelerikReportFactory>();
+builder.Services.AddScoped<ITelerikSnapshotReportFactory, DynamicTelerikSnapshotReportFactory>();
+builder.Services.AddScoped<IReportSourceResolver, SnapshotReportSourceResolver>();
 builder.Services.AddScoped<IReportConnectionStringResolver, ReportConnectionStringResolver>();
 builder.Services.AddScoped<IReportRenderService, TelerikReportRenderService>();
 
